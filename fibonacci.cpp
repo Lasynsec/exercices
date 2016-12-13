bb36c4b5dd1221c4bcf2d306546a9a210f0f2437
herve <herve.mbilo@gmail.com>
2016-12-13 21:39:49 +0100

    other other other

--- fibonacci.cpp
+++ fibonacci.cpp
@@ -4,11 +4,26 @@
 using namespace std;
 
 //prototype
-int Fibonocci(int n);
+int Fibonacci(int n);
 
 int main()
 {
-	
+	int F(0);
+	F = Fibonacci(3);
+
+	cout << F << endl;
 	return 0;
+}
+
+int Fibonacci(int n)
+{
+	if(n <= 1)
+	{
+		cout << " n" << " = " << n << endl;
+		return n;
+	}
+   
+cout << Fibonacci(n-1) << " + " <<Fibonacci(n-2)  <<" = " << Fibonacci(n-1) + Fibonacci(n-2) << endl;
+	return Fibonacci(n-1) +  Fibonacci(n-2);
 }
 
