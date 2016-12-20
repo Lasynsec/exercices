eabf1959fe9ce67914cf2cf9c2182568a9e72e00
M'BILO HERVÉ <2207hembilo@domepfc.net>
2016-12-20 15:40:15 +0100

    finishing the while loop:wq

--- fibonacci.cpp
+++ fibonacci.cpp
@@ -5,13 +5,12 @@ using namespace std;
 
 //prototype
 int Fibonacci(int n);
-int recursTest(int n);
 int askNumber();
 
 int main()
 {
 	int F(0);
-	int askIt;
+	int askIt(0);
 	
 	askIt = askNumber();
 	F = Fibonacci(askIt);
@@ -20,6 +19,9 @@ int main()
 	return 0;
 }
 
+/*
+return the nth value of fiboncci.
+*/
 int Fibonacci(int n)
 {
 	if(n <= 1)
@@ -30,19 +32,25 @@ int Fibonacci(int n)
 	return Fibonacci(n-1) +  Fibonacci(n-2);
 }
 
+/*
+return the user value.
+*/
 int askNumber()
 {
-	}
-	   cout << " Write a number between 0 and 40 !!";
-	   int number(0);
+	int number;
+	int increment(0);
+
+	do{
+	   cout << " Write a number between 0 and 40 !! : ";
 	   cin >> number;
 	   cout << endl;
 
-	   if()
+	   if(number < 1)
 	   {
-	   	
+	   	cout << " The number must be greater than 0 " << endl;	
 	   }
-	}while();
-
+       
+	}while(number < 1); // while number is not < 1
+	
 	return number;
 }
