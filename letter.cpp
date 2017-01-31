6b2f7662fdb81471ac4a1b882457bbb61698679e
M'BILO HERVÉ <2207hembilo@domepfc.net>
2017-01-31 10:35:06 +0100

    yes

---
+++ letter.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+//prototype
+void lettersGenerator();
+
+int main(){
+	
+	lettersGenerator();	
+	return 0;
+}
+
+void lettersGenerator(){
+	string letter;
+	cout << "Good morning deer Mireille,"<<endl;
+	cout << "I\'m writing concerning your last course." << endl;
+	cout << "We should meet up on 12/18 to talk about it" << endl;
+
+}
