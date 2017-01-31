e2f8fbf4342a8b7f93adf9810d4469eb7d1d583a
herve <herve.mbilo@gmail.com>
2017-01-31 20:30:44 +0100

    letter program finished

--- letter.cpp
+++ letter.cpp
@@ -4,18 +4,19 @@
 using namespace std;
 
 //prototype
-void lettersGenerator();
+void lettersGenerator(string name, string subject, string date);
 
 int main(){
 	
-	lettersGenerator();	
+	lettersGenerator("Jhon"," last course","18/12");	
 	return 0;
 }
 
-void lettersGenerator(){
+void lettersGenerator(string name, string subject, string date){
 	string letter;
-	cout << "Good morning deer Mireille,"<<endl;
-	cout << "I\'m writing concerning your last course." << endl;
-	cout << "We should meet up on 12/18 to talk about it" << endl;
-
+	cout << "Good morning deer " << name <<endl;
+	cout << "I\'m writing concerning your "<< subject << endl;
+	cout << "We should meet up on " << date << " to talk about it" << endl;
+	cout << "I'm looking forward to hear about you!"<<endl;
+	cout << "Kindest regards, Herve"<<endl;
 }
