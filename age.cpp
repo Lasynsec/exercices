06565e6574c2c1466947483659c80ab3c52e0517
M'BILO HERVÉ <2207hembilo@domepfc.net>
2017-01-31 09:23:47 +0100

    next next next

--- age.cpp
+++ age.cpp
@@ -5,11 +5,12 @@ int main()
 {	
 	cout << " How old are you ?" << endl;
 	int age(0);					// will receive the user input.
-	while(!(cin>>age)){			// ask until the user type a number.
-		cin.clear();			// clear the error buffer.
-		cin.ignore(1000,'\n');	// ignore it in memory address.
+	while(!(cin>>age)){				// ask until the user type a number.
+		cin.clear();				// clear the error buffer.
+		cin.ignore(1000,'\n');			// ignore it in memory address.
 		cout << "It must be a number !!!"<<endl;
 	}
+
 	int birthYear;
 	birthYear = 2016 - age;
 	cout << "you are born in " << birthYear << endl;
