5d1660e4fb25337ae342fdec314bdced4367ac4b
boloman <herve.mbilo@gmail.com>
2017-02-10 15:13:13 +0100

    the program is working

--- exchange.cpp
+++ exchange.cpp
@@ -1,17 +1,19 @@
 #include <iostream>
 using namespace std;
 
-
+//prototytpes
 void alternator(int& first, int& second);
+int askIt();
 
 int main()
 {
-	int first(30);
-	int second(50);
-
+	int first(askIt());
+	int second(askIt());
+	
 	cout << " Before : " << first << " and " << second << endl;
 	alternator(first,second); 	
 	cout << " After : " << first << " and " << second << endl;
+
 	return 0;
 }
 
@@ -24,3 +26,17 @@ void alternator(int& first, int& second)
 	second = temp;
 }
 
+int askIt()
+{
+	cout << " Type a number : ";
+	int nbr(0);
+	while(!(cin>>nbr))
+	{
+		cout << "It must be a number !!! "<< endl;
+		cin.clear();
+		cin.ignore(1000,'\n');
+	}
+	cout << endl;
+	
+	return nbr;
+}
