58e189aaaf319e8a7ee766f9e7264ae7adb6face
herve <herve.mbilo@gmail.com>
2017-02-14 22:03:08 +0100

    keep working on the askQuestions function.

--- qcm.cpp
+++ qcm.cpp
@@ -13,7 +13,8 @@ struct Qcm{
 
 //Prototypes
 void display(Qcm const& qcm);
-
+int askNumber(int min, int max);
+int askQuestions(Qcm& qcm);
 
 int main(){
 	Qcm qcm = {"How many theeth does an elephant have ?",{"32", "from 6 to 10","to many","24","2"},2};	
@@ -33,3 +34,24 @@ void display(Qcm const& qcm){
 	}
 }
 
+int askNumber(int min, int max){
+	int userInput;
+	do{
+		cout << "Enter a number between "<< min << " and " << max << "endl;
+		if(!(cin>>userInput)){
+			cout << "Please enter numbers only !"<< endl;
+			cin.clear();
+			cin.ignore(1000,'\n');
+		} else if(userInput < min || userInput > max){
+			cout <<"Your answer is not in the answers listed below." << endl;
+		}
+	}while((min > userInput) || (max < userInput));
+
+	return userInput;
+}
+	
+int askQuestions(Qcm& qcm){
+	cout << " Write the question."<<endl;
+	cin<<qcm.question;
+
+}
